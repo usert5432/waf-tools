d646d4671f5ef2ced4470c46ed51f5a7908c7613
Brett Viren <brett.viren@gmail.com>
2017-03-24 16:38:18 -0400

    Add kludge to use slower double precision FFTW as an option.

--- fftw.py
+++ fftw.py
@@ -10,6 +10,8 @@ def options(opt):
                    help="give FFTW3 include installation location")
     opt.add_option('--with-fftw-lib', type='string', default='',
                    help="give FFTW3 lib installation location")
+    opt.add_option('--fftw-no-single',  default=False,
+                   help="set if your FFTW3 does not have single-precision libfftw3f")
 
 
 @conf
@@ -31,9 +33,18 @@ def check_fftw(ctx, mandatory=True):
         if ctx.options.with_fftw_lib:
             ctx.env.LIBPATH_FFTW = [ctx.options.with_fftw_lib]
 
+    if ctx.options.fftw_no_single:
+        # For now, allow use of float/double/float casting for FFTs at the
+        # expense of 15% slower FFTs but to work around missing library at
+        # Fermilab so that MicroBooNE can do its processing ASAP.
+        print 'Note: will use slower double-precision FFTW3 library ',
+        ctx.env.DEFINES_FFTW = ['MISSING_FFTW_SINGLE_PRECISION']
+        ctx.env.LIB_FFTW += ['fftw3']
+    else:
+        ctx.env.LIB_FFTW += ['fftw3f']
+
     ctx.check(header_name="fftw3.h", use='FFTW', mandatory=mandatory)
-    # need to explicitly add floating point version of lib
-    ctx.env.LIB_FFTW += ['fftw3f']
+
     if len(ctx.env.INCLUDES_FFTW):
         ctx.end_msg(ctx.env.INCLUDES_FFTW[0])
     else:
